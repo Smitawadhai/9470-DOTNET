cca6f672728ef9915d5c9beb400a6ce02f2cd074
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-13 20:35:37 +0530

    Add files via upload

---
+++ Anudip Assignments/Special opretor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex06_Spe_oper_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(-9 + 15 * 72);
+            Console.WriteLine(16 - 44 * 8 / 56);
+            Console.WriteLine((74 - 81) % 8);
+            Console.WriteLine(-9*4+95/80);
+        }
+    }
+}
---
+++ Anudip Assignments/Swap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07_Swap_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1,  number2, temp;
+            Console.WriteLine("\nInput the first number :");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nInput the second number:");
+            number2 = int.Parse(Console.ReadLine());
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.WriteLine("\nAffter Swapping:");
+            Console.WriteLine("\nFirst number :" + number1);
+            Console.WriteLine("\nSecond  number :" + number2);
+            Console.ReadLine();
+
+        }
+    }
+}
