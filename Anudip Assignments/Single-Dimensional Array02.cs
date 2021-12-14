b63677ab0dce6677172c2dfe289075212e69d08e
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-14 12:37:58 +0530

    Add files via upload

---
+++ Anudip Assignments/Single-Dimensional Array02.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[] { "c++", "java", "c#" };
+            foreach(string a in arr)
+            {
+                Console.WriteLine(a);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
