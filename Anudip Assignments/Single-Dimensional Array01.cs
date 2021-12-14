72cad7ed17fd87d6f6e3f1888e18d785bc7d21a7
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-14 12:36:32 +0530

    Add files via upload

---
+++ Anudip Assignments/Single-Dimensional Array01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 10, 11, 100, 101, 110 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
