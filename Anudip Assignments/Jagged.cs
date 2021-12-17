cc74d27e4e674707f48719d92c550ec931e5bba9
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-17 11:51:56 +0530

    Add files via upload

---
+++ Anudip Assignments/Jagged.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          int[][] arr = new int[][]
+         {
+          new int[] { 5, 6},
+          new int[] { 15, 17 },
+          new int[] { 117, 122, 188, 111 }
+ };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    System.Console.Write(arr[i][j] + " ");
+                }
+                System.Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
