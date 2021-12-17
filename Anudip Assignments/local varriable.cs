d5c3b5d328a60ec0ca5698e21d5796ecfb8f044c
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-17 11:55:08 +0530

    Add files via upload

---
+++ Anudip Assignments/local varriable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 30;
+            var s = "javatpoint";
+            var arr = new[] { 1, 2, 3 };
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+            Console.WriteLine(arr[2]);
+        }
+    }
+}
