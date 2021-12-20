e03f04520e76e708190a32f0dfa0a69ae20e6284
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-20 17:02:04 +0530

    Add files via upload

---
+++ Anudip Assignments/Is_Operator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace is_operator
+{
+   
+        
+        using System;
+            
+        class One { }
+        class Two { }
+        public class Demo
+        {
+            public static void Test(object obj)
+            {
+                One x;
+                Two y;
+                if (obj is One)
+                {
+                    Console.WriteLine("Class One");
+                    x = (One)obj;
+                }
+                else if (obj is Two)
+                {
+                    Console.WriteLine("Class Two");
+                    y = (Two)obj;
+                }
+                else
+                {
+                    Console.WriteLine("None of the classes!");
+                }
+            }
+            public static void Main()
+            {
+                One o1 = new One();
+                Two t1 = new Two();
+                Test(o1);
+                Test(t1);
+                Test("str");
+                Console.ReadKey();
+            }
+    }
+}
+        
