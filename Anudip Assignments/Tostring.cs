ada23ec189a0251e4700ece3e01ae0f04996b888
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-21 09:56:19 +0530

    Add files via upload

---
+++ Anudip Assignments/Tostring.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tostring_Array
+
+{
+
+    using System;
+    using System.Text;
+    namespace object_class_string
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string name = "Smita Vishweshwar Wadhai";
+                int age = 24;
+                float marks =90.34f;
+                Console.WriteLine(name);
+                Console.WriteLine(age.ToString());
+                Console.WriteLine(marks.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
