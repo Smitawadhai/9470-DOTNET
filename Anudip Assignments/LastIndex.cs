827b8cf1929e516b77618bc6d20b3a1ce74857c4
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-22 09:40:35 +0530

    Add files via upload

---
+++ Anudip Assignments/LastIndex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+
+    using System;
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string a = "CodesDope";
+            Console.WriteLine(a.LastIndexOf('o'));
+        }
+    }
+}
