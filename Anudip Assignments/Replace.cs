dbbfccc96b2e579d2dab351f7cc89dea57e673c5
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-22 09:41:11 +0530

    Add files via upload

---
+++ Anudip Assignments/Replace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    using System;
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string a = "CodesDope";
+            string b = a.Replace('o', 'd');
+            Console.WriteLine(b);
+        }
+    }
+}
