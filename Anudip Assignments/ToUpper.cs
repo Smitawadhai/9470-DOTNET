9240c0fa4b4b3a54aa11b23be9ba2e66f2a70370
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-22 10:37:25 +0530

    Add files via upload

---
+++ Anudip Assignments/ToUpper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    using System;
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string a = "CodesDope";
+            string b = a.ToUpper();
+            Console.WriteLine(b);
+        }
+    }
+}
