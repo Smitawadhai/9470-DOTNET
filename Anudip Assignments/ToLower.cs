85cd5fee7c755460efa5d963ef1e39781162d625
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-22 10:36:22 +0530

    Add files via upload

---
+++ Anudip Assignments/ToLower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    using System;
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string a = "CodesDope";
+            string b = a.ToLower();
+            Console.WriteLine(b);
+        }
+    }
+
+}
