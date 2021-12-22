0f746a93c801de1fb47ac4d18b13f3dca299d3c7
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-22 10:42:37 +0530

    Add files via upload

---
+++ Anudip Assignments/Substring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    using System;
+    class Test
+    {
+        
+
+      static void Main(string[] args)
+        {
+            string a = "CodesDope";
+            string b = a.Substring(5);
+            Console.WriteLine(b);
+        }
+    }
+
+}
