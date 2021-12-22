0a053732774c62bdac4043ebfe05f6a5ca51e18f
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-22 09:37:44 +0530

    Add files via upload

---
+++ Anudip Assignments/Compare.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    using System;
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string a = "Codes";
+            string b = "Dope";
+            int x = String.Compare(a, b);
+            int y = String.Compare(b, a);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
