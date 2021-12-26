16d48560ba6d51cccc1ac6d251f357a64a4d7abd
Smitawadhai <94526885+Smitawadhai@users.noreply.github.com>
2021-12-26 18:58:47 +0530

    Add files via upload

---
+++ Anudip Assignments/Structs with Method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    using System;
+    struct str
+    {
+        int a, b;
+        public void get()
+        {
+            Console.WriteLine("Enter a No.");
+            a = Int32.Parse(Console.ReadLine());
+            b = Int32.Parse(Console.ReadLine());
+        }
+        public void show()
+        {
+            int c = a + b;
+            Console.WriteLine("Sum = " + c);
+        }
+    }
+    class abc
+    {
+        public static void Main()
+        {
+            str obj = new str();
+            obj.get();
+            obj.show();
+        }
+
+    }
+
+}
